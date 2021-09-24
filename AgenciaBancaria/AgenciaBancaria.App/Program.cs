425b305241cc3f27ec5e676582e084813d31352e
Elive@LAPTOP-E7LPKBOO <elivelton-de-jesus@hotmail.com>
2021-09-24 15:35:26 -0300

    Subindo aula da .Net sobre POO

---
+++ AgenciaBancaria/AgenciaBancaria.App/Program.cs
@@ -0,0 +1,38 @@
+﻿using AgenciaBancaria.Dominio;
+using System;
+
+namespace AgenciaBancaria.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Endereco endereco = new Endereco("Rua 14", "123456789", "Sao vicente", "SP");
+                Cliente cliente = new Cliente("Elivelton", "123456", "456789", endereco);
+
+                ContaCorrente conta = new ContaCorrente(cliente, 100);
+
+                Console.WriteLine("Conta" + conta.Situacao + ": " + conta.NumeroConta + "-" +
+                    conta.DigitoVerificador);
+
+                string senha = "adc123456789";
+                conta.Abrir(senha);
+
+                Console.WriteLine("Conta" + conta.Situacao + ": " + conta.NumeroConta + "-" +
+                    conta.DigitoVerificador);
+
+                conta.Sacar(10, senha);
+
+                Console.WriteLine("Saldo:" + conta.Saldo);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ AgenciaBancaria/AgenciaBancaria.Dominio/SituacaoConta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenciaBancaria.Dominio
+{
+    public enum SituacaoConta : int
+    {
+        Criada = 0,
+        Aberta = 1,
+        Encerrada = 2
+    }
+}
